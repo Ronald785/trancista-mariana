781113c0ccdae808fd25a25984f1c8f96bced1af
Ronald Almeida <ronaldmateus785@gmail.com>
2024-07-30 20:31:30 -0300

    Update WhatsApp Message

--- src/components/WhatsApp/index.tsx
+++ src/components/WhatsApp/index.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export const WHatsApp = () => {
     return (
         <Link
-            href="https://wa.me/5519992109540"
+            href="https://wa.me/5519992109540?text=Ol%C3%A1%20Mari.%20Gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es%20sobre%20as%20tran%C3%A7as"
             title="WhatsApp Link"
             target="_blank"
             rel="noopener noreferrer"
